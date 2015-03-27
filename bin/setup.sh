6d87dad713acff118a82131a5dce81f8845dff1f
MetroMini <metromini@mjgnms.mjg.co.id>
2015-03-27 23:48:08 +0700

    install Vundle first

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p $HOME/.vim
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 cp vim/_vimrc $HOME/.vimrc
 cp -r vim/filetype.vim $HOME/.vim/
 cp -r vim/after $HOME/.vim/
